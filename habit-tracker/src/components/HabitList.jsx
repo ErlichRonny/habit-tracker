771dab8dc43f309bee0f899fe3223128d6c2dc0f
Ronny Erlich <ronnyerlich2023@gmail.com>
2025-05-03 17:55:11 -0700

    Add modal started

--- habit-tracker/src/components/HabitList.jsx
+++ habit-tracker/src/components/HabitList.jsx
@@ -1,5 +1,7 @@
 import HabitCard from "./HabitCard";
 import { useState } from "react";
+import HabitModal from "./HabitModal";
+import AddHabitForm from "./AddHabitForm";
 
 function HabitList() {
   const [habits, setHabits] = useState([
@@ -7,20 +9,24 @@ function HabitList() {
     { name: "Exercise", category: "Wellness", streak: 2, id: 2 },
   ]);
 
+  const [showAddModal, setShowModal] = useState(false);
+
   const handleDelete = (habitName) => {
-    setHabits(habits.filter(habit => habit.name !== habitName));
+    setHabits(habits.filter((habit) => habit.name !== habitName));
   };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-start p-6 bg-gray-50">
       <h1 className="text-2xl font-bold mb-6">Habits</h1>
+      <button onClick={() => setShowModal(true)}> Add Habit </button>
+      {showAddModal && (
+        <HabitModal onClose={()=> setShowModal(false)}>
+          <AddHabitForm />
+        </HabitModal>
+      )}
       <div className="flex flex-col items-center gap-4 w-full">
         {habits.map((habit) => (
-          <HabitCard
-            key={habit.id}
-            {...habit}
-            onDelete={handleDelete}
-          />
+          <HabitCard key={habit.id} {...habit} onDelete={handleDelete} />
         ))}
       </div>
     </div>
