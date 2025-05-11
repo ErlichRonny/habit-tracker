ddc99d5e902e56552992024e8c6d70fc584eae79
Ronny Erlich <ronnyerlich2023@gmail.com>
2025-05-10 20:38:34 -0700

    Can unclick category

--- habit-tracker/src/components/CategoryFilter.jsx
+++ habit-tracker/src/components/CategoryFilter.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function CategoryFilter({
   categories,
   filteredCategory,
   setFilteredCategory,
 }) {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = (cat) => {
+    if (!clicked) {
+      setFilteredCategory(cat);
+      setClicked(true);
+    } else {
+      setFilteredCategory("");
+      setClicked(false);
+    }
+  };
+
   return (
     <div className="flex gap-2 mb-4">
       <button
@@ -19,7 +32,7 @@ export default function CategoryFilter({
           className={`px-3 py-1 rounded-full text-sm ${
             filteredCategory === cat ? "bg-blue-200" : "bg-gray-100"
           }`}
-          onClick={() => setFilteredCategory(cat)}
+          onClick={() => handleClick(cat)}
         >
           {cat}
         </button>
