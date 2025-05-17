79b3f2f4fa93621e7ff60a41926943203edfe70e
Ronny Erlich <ronnyerlich2023@gmail.com>
2025-05-16 18:35:34 -0700

    Toggle calendar view

--- habit-tracker/src/App.jsx
+++ habit-tracker/src/App.jsx
@@ -1,15 +1,20 @@
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
+import React, { useState } from 'react';
+import CalendarPage from './components/CalendarPage';
 
 function App() {
+  const [showCalendar, setShowCalendar] = useState(false); 
+  const handleContentToggle = () => {
+    setShowCalendar(!showCalendar);
+  };
   return (
     <div>
-      <Navbar/>
-      <Dashboard />
+      <Navbar handleContentToggle={handleContentToggle} />
+      {showCalendar ? <CalendarPage /> : <Dashboard handleContentToggle={handleContentToggle} />}
     </div>
   );
 }
 
 export default App;
 
-// todo: are you sure you want to delete, habit id for deletion
--- habit-tracker/src/components/Navbar.jsx
+++ habit-tracker/src/components/Navbar.jsx
@@ -1,5 +1,11 @@
-function Navbar(){
-    return <h1 className="text-3xl font-bold underline text-center"> Navbar </h1>;
+function Navbar({ handleContentToggle }) {
+    
+  return (
+    <div className="flex justify-center items-center gap-2 mb-4">
+      <h1 className="text-3xl font-bold text-center" onClick={handleContentToggle}> Dashboard </h1>
+      <button className="text-3xl text-center" onClick={handleContentToggle}> Calendar View </button>
+    </div>
+  );
 }
 
 export default Navbar;
