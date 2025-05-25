1d0a4916c0b9270054c1a830d2390affd9878555
Ronny Erlich <ronnyerlich2023@gmail.com>
2025-05-24 17:51:34 -0700

    Improved navbar look

--- habit-tracker/src/components/HabitModal.jsx
+++ habit-tracker/src/components/HabitModal.jsx
@@ -2,12 +2,12 @@ import { createPortal } from "react-dom";
 
 export default function HabitModal({ children, onClose }) {
   return createPortal(
-    <div className="fixed inset-0 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded shadow-md relative">
+    <div className="fixed inset-0 flex justify-center items-center z-50 bg-black/30">
+      <div className="bg-white w-full max-w-xl mx-4 p-6 rounded-lg shadow-lg relative">
         {children}
         <button
           onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 mb-2"
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-xl"
         >
           ✖
         </button>
--- habit-tracker/src/components/Navbar.jsx
+++ habit-tracker/src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 function Navbar({ onDashboardClick, onCalendarClick, currentView }) {
   return (
-    <div className="flex justify-center items-center gap-2 mb-4">
-      <h1
-        className={`text-2xl font-bold text-center cursor-pointer transition-colors duration-200 ${
+<nav className="flex justify-center items-center gap-4 py-4 border-b border-gray-200 bg-white shadow-sm">
+      <button className={`text-2xl font-bold text-center cursor-pointer transition-colors duration-200 ${
           currentView === "dashboard"
             ? "text-gray-700"
             : "text-gray-500 hover:text-gray-400"
@@ -10,7 +9,7 @@ function Navbar({ onDashboardClick, onCalendarClick, currentView }) {
         onClick={onDashboardClick}
       >
         Dashboard
-      </h1>
+      </button>
       <button
         className={`text-2xl font-bold text-center cursor-pointer transition-colors duration-200 ${
           currentView === "calendar"
@@ -21,7 +20,7 @@ function Navbar({ onDashboardClick, onCalendarClick, currentView }) {
       >
         Calendar View
       </button>
-    </div>
+    </nav>
   );
 }
 
